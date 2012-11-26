19e0473fb8de08e554a2acbe5d6886e6d89e285f
Matt Andreko <mandreko@gmail.com>
2012-11-25 20:37:27 -0500

    Added initial graphing

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'simple-rss'
 gem 'open-uri'
+gem 'googlecharts'
--- sabgraph.rb
+++ sabgraph.rb
@@ -1,11 +1,12 @@
 require 'simple-rss'
 require 'open-uri'
+require 'googlecharts'
 
 def format_speed(number, unit)
   if unit == "MB/s"
     number = number * 1024
   end
-  return "#{number}"
+  return number
 end
 
 # Verify arguments. Show usage and exit on failure
@@ -14,17 +15,24 @@ unless ARGV.length == 4
   exit
 end
 
+data = []
 begin
   rss = SimpleRSS.parse open("http://#{ARGV[0]}:#{ARGV[1]}/rss?mode=history&ma_username=#{ARGV[2]}&ma_password=#{ARGV[3]}&limit=20000")
 
-  puts "Date, Speed (KB/s)"
   # I'm not sure why some items have a nil description, but filter them out, since we can't read their speed
-  rss.items.select{|x| !x.description.nil? }.each do |i|
+  rss.items.select{|x| !x.description.nil? }.sort{|x,y| x.pubDate <=> y.pubDate }.each do |i|
     line = i.description.scan(/Downloaded in .* at an average of (.*)([mk]b\/s)/i).first
     speed = format_speed(line[0].to_f, line[1])
-    puts "#{i.pubDate}, #{speed}"
+    data << speed
   end
 rescue
   puts "Download of the RSS feed failed. Verify your host details and credentials are correct."
 end
 
+chart = Gchart.line(:size => '600x400',
+            :title => "Speed over time in KB/s",
+            :bg => 'efefef',
+            :legend => ['Speed'],
+            :data => data)
+
+puts chart
